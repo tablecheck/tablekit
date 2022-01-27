e5ff57825c852cebe9283b692fb071a7651e3c90
Joan Mira <joan@tablecheck.com>
2022-01-27 18:44:06 +0900

    fix(tablekit-password-field): export stateless version

--- packages/password-field/src/index.ts
+++ packages/password-field/src/index.ts
@@ -1,3 +1,4 @@
 export { PasswordField } from './PasswordField';
+export { PasswordFieldStateless } from './PasswordFieldStateless';
 export { I18nMessageFlag, ScoreLevel } from './types';
 export { calculateScore, validationFunc } from './utils';
