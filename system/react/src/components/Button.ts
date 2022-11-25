d2ad36b19ff66f0e1c420b91329a00e8baf29e25
irmir <irina.soupel@gmail.com>
2022-11-25 10:39:16 +0300

    fix: replace input attributes, fix button padding, focus styles

--- system/react/src/components/Button.ts
+++ system/react/src/components/Button.ts
@@ -17,14 +17,16 @@ export const classySelector = '.btn';
 export const baseStyles = css`
   position: relative;
   display: grid;
-  padding: 12px 20px;
+  padding: 9px var(--spacing-l3);
   grid-gap: var(--spacing-l2);
   grid-auto-flow: column;
+  white-space: nowrap;
   cursor: pointer;
 
   font-weight: 500;
   font-size: 16px;
-  line-height: 24px;
+  line-height: 20px;
+  border: 1px solid var(--primary);
   border-radius: var(--border-radius-small);
 
   align-items: center;
@@ -36,13 +38,25 @@ export const baseStyles = css`
     &:after {
       content: '';
       position: absolute;
-      top: -4px;
-      bottom: -4px;
-      left: -4px;
-      right: -4px;
-      border-radius: var(--border-radius-large);
+      top: -3px;
+      bottom: -3px;
+      left: -3px;
+      right: -3px;
+      border-radius: 6px;
       border: 2px solid var(--focus, hsla(219, 78.5%, 52.5%, 1));
     }
+  }
+
+  &[data-size='small'] {
+    padding: 7px var(--spacing-l2);
+    font-size: 14px;
+    line-height: 16px;
+  }
+
+  &[data-size='large'] {
+    padding: 13px var(--spacing-l4);
+    font-size: 16px;
+    line-height: 20px;
   }
 `;
 
@@ -72,36 +86,66 @@ const variantStyles: Record<ButtonVariant, SerializedStyles> = {
   primary: css`
     --color: white;
     --background-color: var(--primary);
+    --border-color: var(--primary);
     &:hover {
       --background-color: var(--primary-hover);
+      --border-color: var(--primary-hover);
     }
     &:active {
       --background-color: var(--primary-active);
+      --border-color: var(--primary-active);
     }
   `,
   secondary: css`
     --color: var(--text-contrast);
     --background-color: var(--secondary);
+    --border-color: var(--secondary);
     &:hover {
       --background-color: var(--secondary-hover);
+      --border-color: var(--secondary-hover);
     }
     &:active {
       --background-color: var(--secondary-active);
+      --border-color: var(--secondary-active);
     }
   `,
   tertiary: css`
     --color: var(--text);
-    border: 2px solid var(--secondary);
+    border-width: 2px;
+    --border-color: var(--secondary);
+    padding: var(--spacing-l2) var(--spacing-l3);
     &:hover {
       --background-color: var(--surface-hover);
+    }
+    &:focus:not(:focus-visible),
+    &:focus-visible {
+      outline: none;
+      &:after {
+        content: '';
+        position: absolute;
+        top: -4px;
+        bottom: -4px;
+        left: -4px;
+        right: -4px;
+        border-radius: 6px;
+        border: 2px solid var(--focus, hsla(219, 78.5%, 52.5%, 1));
+      }
     }
     &:active {
       --background-color: var(--surface-active);
     }
+
+    &[data-size='small'] {
+      padding: 6px var(--spacing-l2);
+    }
+
+    &[data-size='large'] {
+      padding: var(--spacing-l3) var(--spacing-l4);
+    }
   `,
   ghost: css`
     --color: var(--text);
-    border: 1px solid var(--border-transparent);
+    --border-color: var(--border-transparent);
     &:hover {
       --background-color: var(--surface-hover);
     }
@@ -111,6 +155,7 @@ const variantStyles: Record<ButtonVariant, SerializedStyles> = {
   `,
   bare: css`
     --color: var(--text);
+    --border-color: transparent;
     &:hover {
       --background-color: var(--surface-hover);
     }
@@ -134,6 +179,7 @@ const coreStyles = css`
   }
   color: var(--color);
   background-color: var(--background-color);
+  border-color: var(--border-color);
   --loading-transition: 300ms ease-in-out;
   transition: color var(--loading-transition),
     background-color var(--loading-transition),
@@ -152,7 +198,7 @@ const coreStyles = css`
     z-index: 2;
   }
   &[aria-busy='true'] {
-    ${spinnerElementStyles}
+    ${spinnerElementStyles};
     color: transparent;
   }
   &[aria-busy='true']:before {
@@ -184,6 +230,7 @@ export const VariantButtons = variants.reduce(
 
 export const Button = styled(ButtonBase)<{
   'data-variant'?: ButtonVariant;
+  'data-size'?: 'small' | 'large';
   'aria-busy'?: boolean;
 }>`
   &:not([data-variant]) {
--- system/react/src/components/InputLikeButton.ts
+++ system/react/src/components/InputLikeButton.ts
@@ -1,19 +1,21 @@
 import styled from '@emotion/styled';
 
-import { Spacing } from '../themeVariables/constants';
-
 import { baseStyles, Props } from './Input';
 
 export const classySelector = 'button.input';
 
 export const InputLikeButton = styled.button<Props>`
   ${baseStyles};
-  display: grid;
-  grid-auto-flow: column;
-  grid-gap: ${Spacing.L2};
+  display: flex;
+  gap: var(--spacing-l2);
   align-items: center;
   justify-content: space-between;
   cursor: pointer;
+
+  & > *:not(svg) {
+    flex: 1;
+    text-align: left;
+  }
 
   &:hover {
     background: var(--surface-hover);
