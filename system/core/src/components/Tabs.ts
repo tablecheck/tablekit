16017c12dd21a71282477e652c2c5536c8b77a85
Simeon Cheeseman <1085899+SimeonC@users.noreply.github.com>
2023-08-16 13:53:28 +0900

    fix: add wrap option to tabs and reduce gap to 8px
    No ticket

--- system/core/src/components/Tabs.ts
+++ system/core/src/components/Tabs.ts
@@ -5,6 +5,7 @@ import { OptionalKeys } from '../typeUtils';
 export const className = 'tabs';
 export interface Props {
   role: 'tablist';
+  'data-wrap'?: boolean;
 }
 
 export type DefaultedProps = OptionalKeys<Props, 'role'>;
@@ -13,5 +14,8 @@ export const defaultProps = { role: 'tablist' };
 export const baseStyles = css`
   display: flex;
   flex-direction: row;
-  gap: var(--spacing-l5);
+  gap: var(--spacing-l2);
+  &[data-wrap='true'] {
+    flex-wrap: wrap;
+  }
 `;
