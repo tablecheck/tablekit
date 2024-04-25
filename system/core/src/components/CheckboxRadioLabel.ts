5f5548383a31feb478e37de317212a6786d48fdd
Alexandr Shostyr <sashashostyr@gmail.com>
2024-04-25 14:23:59 +0300

    fix: disabled styles for Checkbox and Radio labels

--- system/core/src/components/CheckboxRadioLabel.ts
+++ system/core/src/components/CheckboxRadioLabel.ts
@@ -3,8 +3,13 @@ import { css } from '@emotion/react';
 export const element = 'label';
 export const selectors = ['label.checkbox', 'label.radio'];
 
+const variants = ['disabled'] as const;
+
+export type CheckboxRadioLabelVariant = (typeof variants)[number];
+
 export interface Props {
   htmlFor?: string;
+  'data-variant'?: CheckboxRadioLabelVariant;
 }
 
 export const baseStyles = css`
@@ -13,4 +18,9 @@ export const baseStyles = css`
   gap: var(--spacing-l2);
   cursor: pointer;
   font: var(--body-1);
+
+  &[data-variant='disabled'], &:has(input:disabled) { {
+    color: var(--text-disabled);
+    cursor: not-allowed;
+  }
 `;
