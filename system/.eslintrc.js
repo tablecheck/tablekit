f58173b46547ceca7c70ad1226acbc9de579387c
SimeonC <1085899+SimeonC@users.noreply.github.com>
2024-05-29 11:30:56 +0900

    fix: package versions CI setup
    Upgrade lerna + nodejs and configurations around those
    Manual bump package numbers due to failing CI

--- system/.eslintrc.js
+++ system/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ['@tablecheck/eslint-config'],
   rules: {
     'jsx-a11y/anchor-has-content': 'off',
-    'react/button-has-type': 'off'
+    'react/button-has-type': 'off',
+    'jsx-a11y/control-has-associated-label': 'off'
   },
   overrides: [
     {
