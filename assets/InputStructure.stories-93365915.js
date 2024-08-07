1a73522d06862601a3923377ccf22fb45acbf808
SimeonC <1085899+SimeonC@users.noreply.github.com>
2024-08-07 08:00:25 +0000

    Deploying to docs from @ tablecheck/tablekit@e738d808b943af0d8a75db9e2413f1216a905734 🚀

---
+++ assets/InputStructure.stories-93365915.js
@@ -0,0 +1,16 @@
+import{j as t,F as S}from"./emotion-react-jsx-runtime.browser.esm-ccbc8680.js";import{T as b,D as a}from"./TextArea-6dc2794c.js";import{I as i,a as I}from"./InputStructure-f6e83625.js";import"./index-b6937ee2.js";import"./Tooltip-c094622b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8c426718.js";import"./index-4d501b15.js";import"./_commonjsHelpers-725317a4.js";const f=[{name:"input-structure-1",label:"Default",input:t(a,{name:"input-structure-1",title:"Default",placeholder:"Placeholder"})},{name:"input-structure-2",label:"With Label",input:t(a,{name:"input-structure-2",title:"Default",placeholder:"Placeholder"}),labelAppend:"Some Label"},{name:"input-structure-3",label:"With Description",input:t(a,{name:"input-structure-3",title:"Default",placeholder:"Placeholder"}),description:"Some Descripton"},{name:"input-structure-4",label:"With Alert",input:t(a,{name:"input-structure-4",title:"Default",placeholder:"Placeholder"}),alert:{children:"Alert","data-variant":"error",id:"1"}},{name:"input-structure-5",label:"With All",input:t(a,{name:"input-structure-5",title:"Default",placeholder:"Placeholder"}),alert:{children:"Alert","data-variant":"error",id:"1"},description:"Some Descripton",labelAppend:"Some Label"}],x={title:"Components/InputStructure",component:i,parameters:{...b}},c=({InputStructure:m})=>t(S,{children:f.map(({name:d,...h})=>t(m,{...h},d))}),e=c.bind({});e.args={InputStructure:i};e.parameters={useEmotion:!0};const r=c.bind({});r.args={InputStructure:I};r.parameters={useEmotion:!1};var n,u,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`({
+  InputStructure
+}) => <>
+    {contentVariants.map(({
+    name: key,
+    ...props
+  }) => <InputStructure key={key} {...props} />)}
+  </>`,...(p=(u=e.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var o,s,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  InputStructure
+}) => <>
+    {contentVariants.map(({
+    name: key,
+    ...props
+  }) => <InputStructure key={key} {...props} />)}
+  </>`,...(l=(s=r.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const C=["Emotion","Class"];export{r as Class,e as Emotion,C as __namedExportsOrder,x as default};
+//# sourceMappingURL=InputStructure.stories-93365915.js.map
