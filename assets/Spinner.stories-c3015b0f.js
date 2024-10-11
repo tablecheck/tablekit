765e71fa2ff94fd2e9cac351025d9bdd0571c1c7
SashaShostyr <sashashostyr@gmail.com>
2024-10-11 09:09:29 +0000

    Deploying to docs from @ tablecheck/tablekit@9a12c934f4591fd625d5bba0a7a85bc5f7cf8691 🚀

--- assets/Spinner.stories-c3015b0f.js
+++
@@ -1,6 +0,0 @@
-import{j as u}from"./emotion-react-jsx-runtime.browser.esm-ccbc8680.js";import{aa as i,ab as S}from"./TextArea-10b25f7a.js";import"./index-b6937ee2.js";import{n as d}from"./Tooltip-224024c2.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8c426718.js";import"./index-4d501b15.js";import"./_commonjsHelpers-725317a4.js";const C={title:"Components/Spinner",component:i,parameters:{...S}},m=({Spinner:c})=>u(c,{"aria-busy":!0}),r=m.bind({});r.args={Spinner:i};r.parameters={useEmotion:!0};const e=m.bind({});e.args={Spinner:d};e.parameters={useEmotion:!1};var s,a,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`({
-  Spinner
-}) => <Spinner aria-busy />`,...(n=(a=r.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var o,t,p;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`({
-  Spinner
-}) => <Spinner aria-busy />`,...(p=(t=e.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};const _=["Emotion","Class"];export{e as Class,r as Emotion,_ as __namedExportsOrder,C as default};
-//# sourceMappingURL=Spinner.stories-c3015b0f.js.map
