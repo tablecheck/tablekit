89064d3aef1bf183e1db9bdb2ffaa5a630cf334f
SimeonC <1085899+SimeonC@users.noreply.github.com>
2024-11-26 06:36:53 +0000

    Deploying to docs from @ tablecheck/tablekit@cf6aa43e426341c8e0ec243ed3a7bc15c3d0d494 🚀

---
+++ assets/Anchor.stories-dbdfaa91.js
@@ -0,0 +1,21 @@
+import{j as o,a as A,F as b}from"./emotion-react-jsx-runtime.browser.esm-3699e87e.js";import{f as r,g as v,h as f}from"./TextArea-01517573.js";import"./index-d8ade8ea.js";import{a as B}from"./Tooltip-53a04026.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-7e8b2c4e.js";import"./index-4d501b15.js";import"./_commonjsHelpers-042e6b4d.js";const s=["Default","Disabled","Hover","Focus","Visited"],j={title:"Components/Anchor",component:r,parameters:{...v,variants:s}},u=({Anchor:e,ButtonAnchor:l})=>A(b,{children:[s.map(t=>o(e,{href:t==="Disabled"?void 0:"#","data-pseudo":t.toLowerCase(),children:"Link text here"})),s.map(t=>o(l,{disabled:t==="Disabled","data-pseudo":t.toLowerCase(),children:"Button text here"}))]}),a=u.bind({});a.args={Anchor:r,ButtonAnchor:r.withComponent("button",{target:"e1l0y1a00"})};const n=u.bind({});n.args={Anchor:B,ButtonAnchor:e=>o("button",{className:f,...e,type:"button"})};var i,c,d;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`({
+  Anchor,
+  ButtonAnchor
+}) => <>
+    {contentVariants.map(variant => <Anchor href={variant === 'Disabled' ? undefined : '#'} data-pseudo={variant.toLowerCase()}>
+        Link text here
+      </Anchor>)}
+    {contentVariants.map(variant => <ButtonAnchor disabled={variant === 'Disabled'} data-pseudo={variant.toLowerCase()}>
+        Button text here
+      </ButtonAnchor>)}
+  </>`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,p,h;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  Anchor,
+  ButtonAnchor
+}) => <>
+    {contentVariants.map(variant => <Anchor href={variant === 'Disabled' ? undefined : '#'} data-pseudo={variant.toLowerCase()}>
+        Link text here
+      </Anchor>)}
+    {contentVariants.map(variant => <ButtonAnchor disabled={variant === 'Disabled'} data-pseudo={variant.toLowerCase()}>
+        Button text here
+      </ButtonAnchor>)}
+  </>`,...(h=(p=n.parameters)==null?void 0:p.docs)==null?void 0:h.source}}};const k=["Emotion","Class"];export{n as Class,a as Emotion,k as __namedExportsOrder,j as default};
---
+++ assets/InputStructure.stories-60988c10.js
@@ -0,0 +1,15 @@
+import{j as t,F as S}from"./emotion-react-jsx-runtime.browser.esm-3699e87e.js";import{U as b,E as a}from"./TextArea-01517573.js";import{I as i,a as f}from"./InputStructure-e946bee2.js";import"./index-d8ade8ea.js";import"./Tooltip-53a04026.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-7e8b2c4e.js";import"./index-4d501b15.js";import"./_commonjsHelpers-042e6b4d.js";const I=[{name:"input-structure-1",label:"Default",input:t(a,{name:"input-structure-1",title:"Default",placeholder:"Placeholder"})},{name:"input-structure-2",label:"With Label",input:t(a,{name:"input-structure-2",title:"Default",placeholder:"Placeholder"}),labelAppend:"Some Label"},{name:"input-structure-3",label:"With Description",input:t(a,{name:"input-structure-3",title:"Default",placeholder:"Placeholder"}),description:"Some Descripton"},{name:"input-structure-4",label:"With Alert",input:t(a,{name:"input-structure-4",title:"Default",placeholder:"Placeholder"}),alert:{children:"Alert","data-variant":"error",id:"1"}},{name:"input-structure-5",label:"With All",input:t(a,{name:"input-structure-5",title:"Default",placeholder:"Placeholder"}),alert:{children:"Alert","data-variant":"error",id:"1"},description:"Some Descripton",labelAppend:"Some Label"}],x={title:"Components/InputStructure",component:i,parameters:{...b}},c=({InputStructure:m})=>t(S,{children:I.map(({name:d,...h})=>t(m,{...h},d))}),e=c.bind({});e.args={InputStructure:i};e.parameters={useEmotion:!0};const r=c.bind({});r.args={InputStructure:f};r.parameters={useEmotion:!1};var n,u,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`({
+  InputStructure
+}) => <>
+    {contentVariants.map(({
+    name: key,
+    ...props
+  }) => <InputStructure key={key} {...props} />)}
+  </>`,...(p=(u=e.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var o,s,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  InputStructure
+}) => <>
+    {contentVariants.map(({
+    name: key,
+    ...props
+  }) => <InputStructure key={key} {...props} />)}
+  </>`,...(l=(s=r.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const C=["Emotion","Class"];export{r as Class,e as Emotion,C as __namedExportsOrder,x as default};
